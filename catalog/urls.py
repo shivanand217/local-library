6ca0bbfa979dc40cd45a9ebd74be47adcced18fb
shiv anand <shivakp2111@gmail.com>
2018-11-19 00:11:26 +0530

    added index templates

--- catalog/urls.py
+++ catalog/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    
+    # name is used to reverse the mapper
+    path('', views.index, name='index'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
 
-# Create your views here.
+from catalog.models import *
+
+# view for the homepage site
+def index(request):
+    
+    # counts to display in the template
+
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+
+    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+    num_authors = Author.objects.all().count()
+
+    context = {
+        'num_books': num_books,
+        'num_instances': num_instances,
+        'num_instances_available': num_instances_available,
+        'num_authors': num_authors,
+    }
+
+    return render(request, 'index.html', context=context)
+
+    
