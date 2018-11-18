1a76a39591a2cde0bf83047081ac273a51d2d44a
shiv anand <shivakp2111@gmail.com>
2018-11-18 22:33:23 +0530

    added inline in admin

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,7 +1,5 @@
 from django.contrib import admin
 
-# Register your models here.
-
 from catalog.models import *
 
 # admin.site.register(Book)
@@ -9,15 +7,38 @@ from catalog.models import *
 # admin.site.register(Author)
 admin.site.register(Genre)
 
+class AuthorInline(admin.TabularInline):
+    model = Book
+    extra = 0
+
+@admin.register(Author)
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ('first_name','last_name','date_of_birth','date_of_death')
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+    inlines = [AuthorInline]
 
-admin.site.register(Author, AuthorAdmin)
+# inlining of models to see Book Instance in admin Book page
+class BookInstanceInline(admin.TabularInline):
+    model = BookInstance
+    extra = 0
+
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_filter = ('status', 'due_back')
+
+    # sectioning the detail view
+
+    fieldsets = (
+        (None, {
+            'fields': ('book', 'imprint', 'id'),
+        }),
+        ('Availability', {
+            'fields': ('status', 'due_back')
+        }),
+    )
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'display_genre')
-
-@admin.register(BookInstance)
-class BookInstanceAdmin(admin.ModelAdmin):
-    pass
+    inlines = [BookInstanceInline]
+    
