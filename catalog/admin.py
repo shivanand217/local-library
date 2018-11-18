5f87a42ac06f635ad0d0ab4cb5231c877e5ff45a
shiv anand <shivakp2111@gmail.com>
2018-11-18 12:51:33 +0530

    some admin customization

--- catalog/admin.py
+++ catalog/admin.py
@@ -4,7 +4,20 @@ from django.contrib import admin
 
 from catalog.models import *
 
-admin.site.register(Book)
-admin.site.register(Author)
+# admin.site.register(Book)
+# admin.site.register(BookInstance)
+# admin.site.register(Author)
 admin.site.register(Genre)
-admin.site.register(BookInstance)
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('first_name','last_name','date_of_birth','date_of_death')
+
+admin.site.register(Author, AuthorAdmin)
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'display_genre')
+
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    pass
--- catalog/models.py
+++ catalog/models.py
@@ -3,8 +3,6 @@ from django.urls import reverse
 
 import uuid # Required for unique book instances
 
-# Create your models here.
-
 class Genre(models.Model):
 
     name = models.CharField(max_length=200,help_text='Enter a book genre (e.g. Science Fiction)')
@@ -12,7 +10,6 @@ class Genre(models.Model):
     def __str__(self):
         # string for representing the model object
         return self.name
-
 
 class Book(models.Model):
 
@@ -27,6 +24,12 @@ class Book(models.Model):
 
     def get_absolute_url(self):
         return reverse('book-detail', args=[str(self.id)])
+
+    # create a string for genre, required to display genre in Admin
+    def display_genre(self):
+        return ', '.join(genre.name for genre in self.genre.all()[:3])
+
+    display_genre.short_description = 'Genre'
 
 # model representing a specific copy of the book that
 # can be borrowed from the library
